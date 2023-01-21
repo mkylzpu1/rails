7cfa2d5f736f786e743b6568f5f8afb914c83dc0
Takaya Saito <mkylzpu123@gmail.com>
2023-01-22 00:02:43 +0900

    devise1バグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions => "users/sessions",
+    :registrations => "users/registrations"
+  }
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+  # devise_scope :user do
+  #   get "/" => "users/sessions#new"
+  #   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "users/sessions#new"
+  authenticated :user do
+    root to: 'users/registrations#edit', as: :user_root
+  end
+
+  # root to: 'users/sessions#new'
 end

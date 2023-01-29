68ee82c57e4b2a54ec58846b5b051d9dfc6db26b
Takaya Saito <mkylzpu123@gmail.com>
2023-01-29 19:07:33 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_143612) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_050248) do
+  create_table "cats", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

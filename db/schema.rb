82c6696b9e4d8c050c4a372738c472fb907dfa49
Takaya Saito <mkylzpu123@gmail.com>
2023-01-29 19:41:47 +0900

    メール機能とプレビュー機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_143612) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_100849) do
+  create_table "cats", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
